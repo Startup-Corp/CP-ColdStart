9ca2f04dc2d7bb3d03c03956029a566a2a19f2ae
Nikita Ivanov <ivanov.n.a.0620@gmail.com>
2024-09-28 16:54:20 +0300

    add csr reader

--- back/model.py
+++ back/model.py
@@ -1,14 +1,22 @@
 import lightgbm
 import pandas as pd
 import numpy as np
+from scipy.sparse import load_npz
 
 class ModelGBM:
-    def __init__(self, path_to_model, dataset: pd.DataFrame ) -> None:
+    def __init__(self, 
+                 path_to_model: str, 
+                 dataset: pd.DataFrame,
+                 csr_path: str = './files/video_csr_matrix.npz') -> None:
         with open(path_to_model, 'r') as file:
             data = file.read()
 
         self.model = lightgbm.Booster(model_str=data)
         self.dataset = dataset
+        self.csr_path = csr_path
+
+    def load_csr(self):
+        return load_npz(self.csr_path)
     
     def candidate_selection(self, rating_vec: np.ndarray, user_features: np.ndarray):
         """
@@ -33,3 +41,10 @@ class ModelGBM:
         
         res_ids = self.dataset[['video_id']].assign(rating=res).sort_values('rating', ascending=False).head(10)['video_id'].values
         return res_ids
+
+
+if __name__ == '__main__':
+    m = ModelGBM('./files/lgbr_v1.txt', pd.DataFrame())
+    csr = m.load_csr()
+    print(type(csr))
+    print(csr)
