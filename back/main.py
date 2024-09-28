eb0114281f46f89dbd16555b00bc873b313e0250
AveZorgen <KulikovArt2015@gmail.com>
2024-09-28 17:34:20 +0300

    base reaction handling

--- back/main.py
+++ back/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from dataset import Dataset
 import numpy as np
 from flask_cors import CORS
+# from collections import defaultdict
 
 data = Dataset('./files/mvp_movie.parquet')
 df = data.get_data_for_pred()
@@ -11,53 +12,68 @@ df = data.get_data_for_pred()
 model = ModelGBM('./files/lgbr_v1.txt', df)
 app = Flask(__name__)
 
-user = np.zeros((1, df.shape[0]))# кол-во фильмов
+# user = pd.DataFrame(0, index=df.index, columns=df.columns)
+user = pd.DataFrame(0, columns=df['video_id'], index=[0])
+recomended = list | None
 
 @app.route('/video', methods=['GET']) # передать доп данные для отображения
 def react():
-    data = request.get_json()
+    inp_data = request.get_json()
     
-    video_id = data.get('id')
+    video_id = inp_data.get('id')
     if not video_id:
         return jsonify({'status': 'no video_id'})
     
-    user[video_id] += 1 # = 1
+    global recomended
+    recomended = None
 
-    return jsonify({'status': 'ok'}) 
+    user[video_id] += 1
+
+
+    return data.get_data_to_view(video_id)
 # title, description, category, likes, dislikes
 
 @app.route('/react', methods=['POST'])
 def react():
-    data = request.get_json()
+    inp_data = request.get_json()
     
-    video_id = data.get('id')
+    video_id = inp_data.get('id')
     if not video_id:
         return jsonify({'status': 'no video_id'})
 
-    rating = data.get('rating', 0) # 0 - dis, 1 - like
+    rating = inp_data.get('rating', 0) # 0 - dis, 1 - like
     
-    user[video_id] += rating
+    if rating == 1:
+        user[video_id] += 2
+    else:
+        user[video_id] = -2
 
     return jsonify({'status': 'ok'})
 
 @app.route('/comment', methods=['POST'])
 def comment():
-    data = request.get_json()
+    inp_data = request.get_json()
     
-    video_id = data.get('id')
+    video_id = inp_data.get('id')
     if not video_id:
         return jsonify({'status': 'no video_id'})
 
-    comment = data.get('comment', 0) # 0 - dis, 1 - like
+    comment = inp_data.get('comment', 0) # 0 - dis, 1 - like
     
     # user[video_id] += rating
 
     return jsonify({'status': 'ok'})
+
 CORS(app)
 
 @app.route('/predict', methods=['GET'])
 def predict():
+    if recomended:
+        for i in recomended:
+            user[i] -= 0.5
+
     ids = model.pred()
+    recomended = ids
     # print(ids)
 
     response = {
