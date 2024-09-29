e0a5d60dfb34f172ac4fbd9cbb72417a5f714dec
Никита Кораблев <kit06092003@gmail.com>
2024-09-29 05:27:19 +0300

    fix date output

--- src/js/VideoCard.js
+++ src/js/VideoCard.js
@@ -3,6 +3,19 @@ import eye from "../Eye.svg"
 
 function VideoCard(rec, index) {
 
+  // Преобразуем строку в объект Date
+  const date = new Date(rec.pub_datetime);
+
+  // Получаем день, месяц и год
+  const day = String(date.getUTCDate()).padStart(2, '0'); // добавляем ведущий 0 для дней меньше 10
+  const month = date.toLocaleString('en-US', { month: 'short' }); // короткое название месяца
+  const year = date.getUTCFullYear();
+
+  // Формируем строку
+  const formattedDate = `${day} ${month} ${year}`;
+
+  const d = formattedDate; // Вывод: 04 Aug 2024
+
   return (
     <a href={'/video?id=' + rec.id} className="card border" key={index}>
       <h3>{rec.title}</h3>
@@ -15,7 +28,7 @@ function VideoCard(rec, index) {
           <p>{rec.views}</p>
         </div>
         <div className='circle' />
-        <p>{rec.post_time}</p>
+        <p>{d}</p>
       </div>
     </a>
   );
